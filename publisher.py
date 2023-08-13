669ab4d6b92a8f7b269c4f05e6c3713c951ed75e
Usama Tahir <usama.tahir@informatik.hs-fulda.de>
2023-08-14 00:57:11 +0500

    modified publisher.py to extract data from bigquerey table and publish row by row on google cloud pub sub topic

--- publisher.py
+++ publisher.py
@@ -1,10 +1,20 @@
 import os
+import pickle
 from google.cloud import pubsub_v1
+from google.cloud import bigquery
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "{Path_Of_GCP_Credentials.json}"
-# Set your Google Cloud project ID and topic name
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "{give_path_to_your_gcp_credentials_file}"
+# Set your Google Cloud project ID and topic name 
 project_id = "austin-bikeshare-data-analysis"
 topic_name = "austin-bikeshare-data"
+
+# Creting bigquerey.Client instance
+client = bigquery.Client()
+dataset_id = "bigquery-public-data.austin_bikeshare"
+table_id = "bigquery-public-data.austin_bikeshare.bikeshare_trips"
+query = f"SELECT * FROM `{table_id}` LIMIT 10"
+query_job = client.query(query)
+results = query_job.result()
 
 # Create a PublisherClient instance
 publisher = pubsub_v1.PublisherClient()
@@ -13,12 +23,11 @@ publisher = pubsub_v1.PublisherClient()
 topic_path = publisher.topic_path(project_id, topic_name)
 
 # Define the message to publish
-message_data = b'Hello, Pub/Sub!'
-
-# Publish the message
-future = publisher.publish(topic_path, message_data)
-print(f"Published message: {message_data}")
-
-# Wait for the publish to complete
-future.result()
-print("Message published.")
+for row in results:
+    message_data = pickle.dumps(row)
+    # Publish the message
+    future = publisher.publish(topic_path, message_data)
+    print(f"Published message: {message_data}")
+    # Wait for the publish to complete
+    future.result()
+    print("Message published.")
